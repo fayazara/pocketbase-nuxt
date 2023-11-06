4f87b03ed19254b9faafd42d20dbedf5f0220590
Fayaz Ahmed <fayazara@gmail.com>
2023-11-06 09:30:06 +0530

    Refactor dashboard page with new post creation modal

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,32 +1,93 @@
 <template>
   <div>
-    <div v-if="user">
-      <p>Name: {{ user.name }}</p>
-      <p>Email: {{ user.email }}</p>
-      <p>Username: {{ user.username }}</p>
-      <UButton @click="logout" label="Logout" color="black" />
-      <UButton @click="refresh" label="Refresh" color="black" />
-      <h4 class="mt-4 font-semibold">Posts</h4>
-      <ul class="mt-8 space-y-4">
+    <div v-if="user" class="space-y-4">
+      <div>
+        <p>Name: {{ user.name }}</p>
+        <p>Email: {{ user.email }}</p>
+      </div>
+      <div class="flex items-center gap-2">
+        <UButton @click="logout" label="Logout" color="black" />
+        <UButton @click="refresh" label="Refresh" color="black" />
+      </div>
+      <div class="flex items-center justify-between">
+        <h4 class="font-semibold">Posts</h4>
+        <UButton @click="newPost = true" label="Create Post" color="black" />
+      </div>
+      <ul class="grid grid-cols-2 gap-4">
         <li v-for="post in posts" :key="post.id">
-          <p>
-            {{ post.title }}
-          </p>
           <div v-if="post.thumbnail">
-            <img :src="getFileUrl(post)" />
+            <img
+              :src="getFileUrl(post)"
+              class="h-40 w-full object-cover rounded-lg"
+            />
           </div>
+          <p>{{ post.title }}</p>
         </li>
       </ul>
     </div>
+    <UModal v-model="newPost">
+      <div class="p-4 space-y-4">
+        <p>Add New Post</p>
+        <UInput v-model="post.title" label="Title" placeholder="Enter title" />
+        <UTextarea v-model="post.body" label="Body" placeholder="Enter body" />
+        <div>
+          <p class="text-sm">Post Preview Image</p>
+          <div
+            ref="dropZoneRef"
+            class="rounded-lg h-40 bg-gray-100 ring-2 ring-gray-200 flex items-center justify-center mt-2"
+            :class="{ 'animate-pulse': isOverDropZone }"
+          >
+            Drop files here
+          </div>
+        </div>
+        <UButton @click="savePost" size="lg" label="Save" color="black" block />
+      </div>
+    </UModal>
   </div>
 </template>
 
 <script setup>
+import { useDropZone } from "@vueuse/core";
+
+const dropZoneRef = ref();
+const toast = useToast();
+const { isOverDropZone } = useDropZone(dropZoneRef, onDrop);
+const newPost = ref(false);
+
+const post = ref({
+  title: undefined,
+  body: undefined,
+  thumbnail: undefined,
+});
+
 const { user } = usePocketBaseUser();
 const pb = usePocketBaseClient();
 definePageMeta({
   middleware: ["auth"],
 });
+
+async function onDrop(files) {
+  post.value.thumbnail = files[0];
+}
+
+async function savePost() {
+  try {
+    post.value.owner = user.value.id;
+    const record = await pb.collection("posts").create(post.value);
+    console.log(record);
+    toast.add({
+      title: "Post created successfully",
+    });
+    post.value = {
+      title: undefined,
+      body: undefined,
+      thumbnail: undefined,
+    };
+    newPost.value = false;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 const { data: posts, refresh } = await useAsyncData(
   "posts",
