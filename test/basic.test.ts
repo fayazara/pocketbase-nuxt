54e4929c2da169cd6ce57d08250e00fb430a795c
Fayaz <fayazara@gmail.com>
2025-02-10 20:17:42 +0530

    Write tests later

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,16 +1,7 @@
-import { fileURLToPath } from 'node:url'
 import { describe, it, expect } from 'vitest'
-import { setup, $fetch } from '@nuxt/test-utils/e2e'
 
-// this is the default test that came pre-configured when you created the project (delete and replace with your own tests)
-describe('ssr', async () => {
-  await setup({
-    rootDir: fileURLToPath(new URL('./fixtures/basic', import.meta.url)),
-  })
-
-  it('renders the index page', async () => {
-    // Get response to a server-rendered page with `$fetch`.
-    const html = await $fetch('/')
-    expect(html).toContain('<div>basic</div>')
+describe('nuxt-pocketbase module', async () => {
+  it('tests', async () => {
+    expect(true).toBe(true)
   })
 })
